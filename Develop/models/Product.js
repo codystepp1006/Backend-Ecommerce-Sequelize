4b1f42c8c9191b339a281daa9f47815cef3f598f
Cody Stepp <codystepp691@gmail.com>
2024-03-04 14:59:57 -0600

    added models, tables.sql file still working on getting it working

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -2,6 +2,7 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+const Category = require('./Category');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -10,6 +11,41 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+        isDecimal: {
+          msg: 'Balance must be a decimal number.'
+        }
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isInt: true,
+        isInt: {
+          msg: 'Stock must be integer.'
+        }
+      }
+
+    },
+    category_id: {
+      type: DataTypes.INTEGER
+
+    }
   },
   {
     sequelize,
@@ -19,5 +55,6 @@ Product.init(
     modelName: 'product',
   }
 );
+Product.hasMany(Category);
 
 module.exports = Product;
--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -1,12 +1,27 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
+const Product = require('./Product');
+const Tag = require('./Tag');
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      // REFERENCES THE PRODUCTS MODEL ID
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      //REFERENCES THE TAG MODELS ID
+    }
   },
   {
     sequelize,
@@ -16,5 +31,5 @@ ProductTag.init(
     modelName: 'product_tag',
   }
 );
-
+Product.belongsToMany(Tag, { through: ProductTag });
 module.exports = ProductTag;
